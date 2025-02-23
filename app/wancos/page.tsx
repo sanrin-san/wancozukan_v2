13e71ef5be0ea49c04ef8806016c535c723fc089
nohiro <nyan.gos4@gmail.com>
2025-02-24 04:41:56 +0900

    update: replace portrait path

--- app/wancos/page.tsx
+++ app/wancos/page.tsx
@@ -1,10 +1,10 @@
 import { createClient } from '@/utils/supabase/server';
 import Image from 'next/image';
+import { Suspense } from 'react';
 
 export default async function Wancos() {
   const supabase = await createClient();
   const { data: wancos, error } = await supabase.from("wancos").select();
-  const portraits = await supabase.storage.from('portraits');
 
   if (error) {
     return <div>wancoの取得でエラーが発生しました</div>
@@ -13,17 +13,17 @@ export default async function Wancos() {
   return (
     <div className="container mx-auto px-4">
      {wancos && wancos.length > 0 && (
-        <ul className="grid grid-cols-3 md:grid-cols-5 gap-4">
+        <ul className="grid grid-cols-3 md:grid-cols-5 gap-2 md:gap-4">
           {wancos.map((wanco) => {
-            const paddedNumber = wanco.number ? String(wanco.number).padStart(3, '0') : '000';
             const { data: publicUrl } = supabase.storage
               .from('portraits')
-              .getPublicUrl(`${paddedNumber}.jpg`);
+              .getPublicUrl(wanco.portrait);
 
             return (
               <li key={wanco.id} className="block">
-                <a href={`/wancos/${wanco.id}`} className="group/card flex flex-col items-center gap-y-1">
-                  <figure className="w-full border border-gray-300 rounded-md overflow-hidden
+                <Suspense fallback={<div>Loading...</div>}>
+                  <a href={`/wancos/${wanco.id}`} className="group/card flex flex-col items-center gap-y-1">
+                    <figure className="w-full border border-gray-300 rounded-md overflow-hidden
                                      group-hover/card:shadow-md transition-shadow duration-300">
                     <Image
                       src={publicUrl.publicUrl}
@@ -34,17 +34,18 @@ export default async function Wancos() {
                       height={300}
                       className="w-full h-full aspect-square object-cover"
                     />
-                  </figure>
-                  <p className="text-xs text-gray-500">
-                    {wanco.name_ja} ({paddedNumber})
-                  </p>
-                </a>
+                    </figure>
+                    <p className="max-md:hidden text-xs text-gray-500">
+                        {wanco.name_ja} ({String(wanco.number).padStart(3, '0')})
+                    </p>
+                  </a>
+                </Suspense>
               </li>
             );
           })}
         </ul>
       )}
-      <pre>{JSON.stringify(wancos, null, 2)}</pre>
+      {/* <pre>{JSON.stringify(wancos, null, 2)}</pre> */}
     </div>
   )
 }
